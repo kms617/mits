6ce55cfe199ef6fd71c6d3ed763e765c7945bf51
Kristin Smith <krysia.codes@gmail.com>
2015-05-05 15:22:54 -0400

    Adds to en.yml file

--- spec/features/create_task_spec.rb
+++ spec/features/create_task_spec.rb
@@ -6,7 +6,7 @@ feature 'user creates a task' do
 
     visit new_task_path
     fill_in "task_name", with: task.name
-    click_button "Create Task"
+    click_button "commit"
 
     expect(page).to have_content("Task successfully submitted")
     expect(page).to have_content(task.name)
@@ -15,9 +15,9 @@ feature 'user creates a task' do
   scenario 'user submits a blank form' do
     visit new_task_path
 
-    click_button "Create Task"
+    click_button "commit"
 
     expect(page).to have_content("There was a problem, please try again.")
-    expect(page).to have_content("Name can't be blank.")
+    expect(page).to have_content("can't be blank.")
   end
 end
